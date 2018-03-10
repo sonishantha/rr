133db906730b4792e048dd80c6fd27802b52371b
ghpranav <bedrepranav@gmail.com>
2018-03-11 01:32:43 +0530

    Tissot: Fork camera provider HAL

--- device.mk
+++ device.mk
@@ -128,14 +128,9 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0_32 \
-    camera.device@1.0-impl \
     camera.device@3.2-impl \
-    camera.device@3.3-impl \
-    libmmcamera_interface \
-    libmmcamera_interface2 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-service.tissot \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
